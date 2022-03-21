bf51b248324d6c78e46bd64227abed4475b55217
wolf <palanivaradharaj7@gmail.com>
2022-03-21 10:06:30 +0530

    auto-reload

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  
-  
+
+  let date = new Date();
+  let minutes = date.getMinutes();
+  // if (minutes > 30) {
+  //   minutes -= 30;
+  // }
+  if (minutes%2 == 0){
+    minutes = 2;
+  }
+  else{
+    minutes = 1;
+  }
+  // let t = `${hours}:${minutes}`;
   res.json({
     i: index,
+    time: minutes,
   });
 });
 
